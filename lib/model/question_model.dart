e5dd401480bc308bc26923615b909f9ea3d1cb5e
kareem358 <kareemullahafridi10@gmail.com>
2025-05-15 10:01:29 -0700

    Create question_model.dart

---
+++ lib/model/question_model.dart
@@ -0,0 +1,25 @@
+// will create a question model here
+
+class Question{
+  // create the structure of the question
+  //every question will get an id
+
+  final String id;
+  // there must be a title of the question so
+  final String title;
+  // options for the question
+  final Map <String, bool> options;
+  // option  will be like - {'1': true, '2':false} = like so
+  Question({
+    required this.id,
+    required this.title,
+    required this.options,
+});
+  // override the tostring method to print the question on console
+  @override
+  String toString() {
+    // TODO: implement toString
+    //return super.toString();
+    return 'Question(id: $id, title: $title, options: $options )';
+  }
+}
