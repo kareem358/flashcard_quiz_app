d24d70794222e1e65790f0100a013fb66c86cf8c
kareem358 <kareemullahafridi10@gmail.com>
2025-05-16 12:15:42 -0700

    Update main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
+// run the main method
+
+void main ()
+{
+  runApp(
+    const MyApp(),// creating MyApp below
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});// any one line can be okay
+  //const MyApp({Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),// creating  this in a separate folder for differnt screen
+    );
+  }
+}
