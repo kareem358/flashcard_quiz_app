ea621ed5a23a441f2359fe7844d285cfb496c979
kareem358 <kareemullahafridi10@gmail.com>
2025-05-16 14:45:27 -0700

    list related issue ... is it final or not to be final
    here check for whether the list is final or not .. it shows some error when not declare as final ...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../model/question_model.dart';
+import '../widgets/question_widget.dart';
 
 // create the HomeScreen widget
 
@@ -15,7 +16,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   //creating a list data for the question
-  List<Question> _questions= [
+  // whether the list is final or not .. check it later on
+final List<Question> _questions= [
     Question(id: '10', title: 'what is 2 +2', options:  {'5':false,
     '6':false, '4':true, '10':false}),
     Question(id: '11', title: 'what is 12 +2', options:  {'5':false,
@@ -34,6 +36,23 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: background,
         shadowColor: Colors.transparent,
         title: Text('Quiz App'),
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+
+          children: [
+            // add the question widget here
+            QuestionWidget(indexAction: index,// currently at zero index
+                question: _questions[index].title,// first  in the list question
+                totalQuestions: _questions.length, // total length of the list
+
+            ),
+
+            const Divider(color: neutral,)
+          ],
+        ),
       ),
     );
   }
