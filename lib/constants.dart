52b035dd98da6795847bbb00c9927f6fa7f700e0
kareem358 <kareemullahafridi10@gmail.com>
2025-05-16 14:42:34 -0700

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,6 @@ const Color correct= Color(0xFF32AB58);
 // set color for incorrect answer
 const Color incorrect= Color(0xFFAB3232);
 // set a netural color
-const Color netural= Color(0xFFE7E7E7);
+const Color neutral= Color(0xFFE7E7E7);
 // set same background color
-const Color background= Color(0xFF7DF1FD);
+const Color background= Color(0xFF034FB1);
