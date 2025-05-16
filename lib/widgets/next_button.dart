275d2163eb64afaea06aefed30b835b29e4c5ddf
kareem358 <kareemullahafridi10@gmail.com>
2025-05-16 15:49:59 -0700

    Create next_button.dart

---
+++ lib/widgets/next_button.dart
@@ -0,0 +1,28 @@
+import 'package:flashcard_appp/constants.dart';
+import 'package:flutter/material.dart';
+
+
+class NextButton extends StatelessWidget {
+  const NextButton({super.key, required this.nextQuestion});
+  final VoidCallback nextQuestion;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: nextQuestion,
+      child: Container(
+        decoration: BoxDecoration(
+          color: neutral,
+          borderRadius: BorderRadius.circular(10.0)
+        ),
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        child: const Text(
+          'Next Question',
+          textAlign: TextAlign.center,
+
+        ),
+      ),
+    );
+  }
+}
