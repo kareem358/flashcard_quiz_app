1bf3e4243b865c147a60f3cb9a0d991267503a57
kareem358 <kareemullahafridi10@gmail.com>
2025-05-17 14:07:17 -0700

    Update option_card.dart

--- lib/widgets/option_card.dart
+++ lib/widgets/option_card.dart
@@ -4,27 +4,25 @@ import '../constants.dart';
 class OptionCard extends StatelessWidget {
   const OptionCard({super.key,
     required this.option,
-    required this.color,
-  required this.onTap,});
+   required this.color,
+
+  });
 
   final String option;
   //final bool isClicked ;
   final Color color;
-  final VoidCallback onTap;
+ // final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Card(
-        color: color,
-        child: ListTile(
-          title: Text(
-            option,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 22.0,
-            ),
+    return Card(  // removing the gesture widget from the card.
+      color: color,
+      child: ListTile(
+        title: Text(
+          option,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 22.0,
           ),
         ),
       ),
