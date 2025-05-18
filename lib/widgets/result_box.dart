36ab6e46835011aed646db7d3cc9d88920874043
kareem358 <kareemullahafridi10@gmail.com>
2025-05-18 07:22:25 -0700

    Update result_box.dart

---
+++ lib/widgets/result_box.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class ResultBox extends StatelessWidget {
+  const ResultBox({super.key, 
+    required this.result,
+    required this.questionLength,
+    
+  });
+  final int result;
+  final int questionLength;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: background,
+      content: Padding(
+          padding: EdgeInsets.all(60.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Result', 
+              style:TextStyle(color: neutral , fontSize: 24.0) ,),
+            const SizedBox(height: 20.0,),
+            CircleAvatar(child: Text("$result/$questionLength",
+            style: TextStyle(fontSize: 22.0),),
+            radius: 70.0,
+              /*backgroundColor: result==questionLength
+                  ? correct
+                  : result < questionLength/2
+                  ? incorrect
+                  : result== questionLength/2
+                  ? Colors.yellow
+                  : Colors.blue,*/
+              backgroundColor: result== questionLength/2
+                  ?Colors.yellow // yellow on 50% marks
+                  :result<questionLength/2
+                   ? incorrect // if less then 50% will show red color
+                  : correct, // on greater than 50% will show green
+            ),
+            const SizedBox(height: 20.0,),
+            Text(
+              result== questionLength/2
+                  ?' Almost there ' // yellow on 50% marks
+                  :result<questionLength/2
+                  ? "good Luck Next time" // if less then 50% will show red color
+                  : "Great job!", // on greater than 50% will show
+              style: TextStyle(color: neutral, fontSize: 20.0),
+            ),
+
+          ],
+        ),
+      
+      ),
+    );
+  }
+}
