dcca2d8cbea9c56cdf96e01281c795b276906736
kareem358 <kareemullahafridi10@gmail.com>
2025-05-18 02:33:24 -0700

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,9 @@
 // color variable and stuff will be store here
 import 'package:flutter/material.dart';
 
-const Color correct= Color(0xFF32AB58);
+const Color correct= Color(0xFF28DD5E);
 // set color for incorrect answer
-const Color incorrect= Color(0xFFAB3232);
+const Color incorrect= Color(0xFFE31818);
 // set a netural color
 const Color neutral= Color(0xFFE7E7E7);
 // set same background color
