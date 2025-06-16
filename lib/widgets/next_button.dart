86349f5d98affb13ec8789928bec250e90e52a46
kareem358 <kareemullahafridi10@gmail.com>
2025-06-16 07:44:44 -0700

    Update next_button.dart

--- lib/widgets/next_button.dart
+++ lib/widgets/next_button.dart
@@ -1,5 +1,5 @@
-import 'package:flashcard_appp/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flashcard_appp/constants.dart';
 
 
 class NextButton extends StatelessWidget {
@@ -9,15 +9,17 @@ class NextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: BoxDecoration(
         color: neutral,
         borderRadius: BorderRadius.circular(10.0)
       ),
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
+
+      padding: const EdgeInsets.symmetric(vertical: 14.0),
       child: const Text(
         'Next Question',
         textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 18.0),
 
       ),
     );
