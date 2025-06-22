649fe973a5bcf9611b92f183d87602864deabc2c
kareem358 <kareemullahafridi10@gmail.com>
2025-06-22 04:42:17 -0700

    Update db_connect.dart

--- lib/model/db_connect.dart
+++ lib/model/db_connect.dart
@@ -1,47 +1,22 @@
-import 'package:http/http.dart' as http; // the http package
-import './question_model.dart';
 import 'dart:convert';
- class DBconnect {
-   // lets first create a function to add question to the database.
-   // here in the brackets we have to add the address of firebase database
-   // declare the name of the table and add .json as suffix after the table name in the end of address
-   final url = Uri.parse('https://flashcardquizapp-a138a-default-rtdb.firebaseio.com/'
-       'question.json');
+import 'package:http/http.dart' as http;
+import './question_model.dart';
 
-// it is required if we want to add any question directly from app.
-   // Future<void> addQuestion(Question question)async{
-   //   http.post(url, body: json.encode({
-   //     'title': question.title,
-   //     'option': question.options,
-   //   }));
-   // }
+class DBconnect {
+  final url = Uri.parse(
+      'https://flashcardquizapp-a138a-default-rtdb.firebaseio.com/question.json');
 
+  Future<List<Question>> fetchQuestion() async {
+    final resp = await http.get(url);
+    final raw = json.decode(resp.body);
+    if (raw is! Map) return [];
 
-    //fetch the data from database
- Future<List<Question>> fetchQuestion() async{
-     // we just got the data using this , now let's print it to see what we got
-    return http.get(url).then((response) {
-       // the 'then' method returns a 'response' which is our data.
-       // what inside will be  decoded first
-
-       var data = json.decode(response.body) as Map<String, dynamic>;
-       List<Question> newQuestions =[];
-       data.forEach((key, value){
-         var newQuestion= Question (
-           id: key,// the encrypted key/ the title we give to our data
-           title : value ['title'],// title of the question
-           options:value['options'] != null
-               ? Map.castFrom(value['options'])
-               : <String, bool>{},
-
-
-         );
-
-         // add to new question
-         newQuestions.add(newQuestion);
-
-       });
-       return newQuestions;
-     });
- }
- }
+    final qs = <Question>[];
+    raw.forEach((key, value) {
+      if (value is Map) {
+        qs.add(Question.fromMap(key.toString(), value));
+      }
+    });
+    return qs;
+  }
+}
