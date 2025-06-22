3a47113c491453637d4eff01827c2f06a8a6691f
kareem358 <kareemullahafridi10@gmail.com>
2025-06-22 04:42:19 -0700

    Update question_widget.dart

--- lib/widgets/question_widget.dart
+++ lib/widgets/question_widget.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 
-
 class QuestionWidget extends StatelessWidget {
-  const QuestionWidget({super.key, required this.question, required this.indexAction,
-  required this.totalQuestions});
-// here we need the question title and the total number of questions,
-  //  and also the index
-  
+  const QuestionWidget({
+    super.key,
+    required this.question,
+    required this.indexAction,
+    required this.totalQuestions,
+  });
+
   final String question;
   final int indexAction;
   final int totalQuestions;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.centerLeft,
-      child:Text('Question ${indexAction + 1}/$totalQuestions: $question',
+      child: Text(
+        'Question ${indexAction + 1}/$totalQuestions: $question',
         style: const TextStyle(
           fontSize: 24,
           color: neutral,
-        ),),
-
+        ),
+      ),
     );
   }
 }
