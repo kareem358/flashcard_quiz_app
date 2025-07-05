2eac2c08780d94ff255efc1220a5ac0236a85746
kareem358 <kareemullahafridi10@gmail.com>
2025-07-06 00:26:22 +0500

    Update db_connect.dart

--- lib/model/db_connect.dart
+++ lib/model/db_connect.dart
@@ -4,7 +4,8 @@ import './question_model.dart';
 
 class DBconnect {
   final url = Uri.parse(
-      'https://flashcardquizapp-a138a-default-rtdb.firebaseio.com/');
+      'https://flashcardquizapp-a138a-default-rtdb.firebaseio.com/question.json');
+
   Future<List<Question>> fetchQuestion() async {
     final resp = await http.get(url);
     final raw = json.decode(resp.body);
@@ -16,6 +17,8 @@ class DBconnect {
         qs.add(Question.fromMap(key.toString(), value));
       }
     });
+    print("Response code: ${resp.statusCode}");
+    print("Response body: ${resp.body}");
     print("Loaded questions count: ${qs.length}");
     return qs;
   }
