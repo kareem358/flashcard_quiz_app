84e0662010a0d7caab181e9a6f6050ee520d3e86
kareem358 <kareemullahafridi10@gmail.com>
2025-07-06 16:04:27 +0500

    Update option_card.dart

--- lib/widgets/option_card.dart
+++ lib/widgets/option_card.dart
@@ -2,6 +2,44 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 
 class OptionCard extends StatelessWidget {
+  const OptionCard({
+    super.key,
+    required this.option,
+    required this.color,
+  });
+
+  final String option;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      elevation: 4.0, // subtle shadow
+      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+        child: Text(
+          option,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: color == neutral ? Colors.black : Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class OptionCard extends StatelessWidget {
   const OptionCard({super.key,
     required this.option,
    required this.color,
@@ -33,3 +71,4 @@ class OptionCard extends StatelessWidget {
     );
   }
 }
+*/
