285ded6bd0078630dfbbe424b4c44f1361381f0a
kareem358 <kareemullahafridi10@gmail.com>
2025-08-21 21:46:14 +0500

    Update db_connect.dart

---
+++ lib/model/db_connect.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import './question_model.dart';
+
+class DBconnect {
+  final url = Uri.parse(
+      'https://flashcardquizapp-a138a-default-rtdb.firebaseio.com/question.json');
+
+  Future<List<Question>> fetchQuestion() async {
+    final resp = await http.get(url);
+    final raw = json.decode(resp.body);
+    if (raw is! Map) return [];
+
+    final qs = <Question>[];
+    raw.forEach((key, value) {
+      if (value is Map) {
+        qs.add(Question.fromMap(key.toString(), value));
+      }
+    });
+
+    return qs;
+  }
+}
