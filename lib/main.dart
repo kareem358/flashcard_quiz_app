bfa6595633afe11afc01f044b78c42fc256b3594
kareem358 <kareemullahafridi10@gmail.com>
2025-08-24 11:36:58 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,3 @@ import './screens/home_screen.dart';
 void main ()
 {
 
-  runApp(
-    const MyApp(),// creating MyApp below
-  );
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});// any one line can be okay
-
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),// creating  this in a separate folder for different screen
-    );
-  }
-}
