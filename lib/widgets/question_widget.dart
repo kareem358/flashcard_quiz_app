c37fb1678431a3560a11f523e85e4f147a696d60
kareem358 <kareemullahafridi10@gmail.com>
2025-08-24 11:36:50 +0500

    Update question_widget.dart

--- lib/widgets/question_widget.dart
+++ lib/widgets/question_widget.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 
+class QuestionWidget extends StatelessWidget {
+  const QuestionWidget({
+    super.key,
+    required this.question,
+    required this.indexAction,
+    required this.totalQuestions,
+  });
+
+  final String question;
+  final int indexAction;
+  final int totalQuestions;
+
   @override
   Widget build(BuildContext context) {
     return Container(
