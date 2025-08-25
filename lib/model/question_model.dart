499dc6d3087cf389710584a077f3a3566617cb5e
kareem358 <kareemullahafridi10@gmail.com>
2025-08-25 15:16:30 +0500

    Update question_model.dart

--- lib/model/question_model.dart
+++ lib/model/question_model.dart
@@ -1,4 +1,30 @@
 
+class Question {
+  final String id;
+  final String title;
+  final Map<String, bool> options;
+
+  Question({
+    required this.id,
+    required this.title,
+    required this.options,
+  });
+
+
+  factory Question.fromMap(String id, Map<dynamic, dynamic> map) {
+    final title = map['title']?.toString() ?? '';
+    final rawOptions = map['options'] as Map<dynamic, dynamic>? ?? {};
+    final parsedOptions = <String, bool>{};
+    rawOptions.forEach((key, value) {
+      parsedOptions[key.toString()] = value == true;
+    });
+
+    return Question(
+      id: id,
+      title: title,
+      options: parsedOptions,
+    );
+  }
 
   @override
   String toString() => 'Question(id: $id, title: $title, options: $options)';
